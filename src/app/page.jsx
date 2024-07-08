242c87cd2b6dd5781573f1eb48e4707de0908b9c
nolimits20 <oladeleakomolede@yahoo.com>
2024-07-08 18:53:38 +0100

    feat: Mobile design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { RxCross2 } from "react-icons/rx";
 import { AiFillSun } from "react-icons/ai";
 import { RiMoonFill } from "react-icons/ri";
+import './component/oladele.module.css'
 
 export default function Home() {
   const [tasks, setTasks] = useState([
@@ -53,7 +54,7 @@ export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-start p-0 ${isDarkMode ? 'bg-gray-800' : 'bg-gray-100'} relative`}>
       {/* Background Image */}
-      <div className="relative w-full h-[50vh]">
+      <div className="relative w-full h-[35vh]">
         <Image
           src={isDarkMode ? "/bg-desktop-dark.jpg" : "/bg-desktop-light.jpg"}
           alt="Background"
@@ -61,45 +62,47 @@ export default function Home() {
           layout="fill"
           objectFit="cover"
         />
-        <div className="relative flex flex-col items-center justify-center h-full w-full">
-          <div className="flex justify-between items-center w-[500px]">
-            <h1 className="text-white text-4xl font-bold">TODO</h1>
-            <button className="text-white text-4xl" onClick={toggleDarkMode}>
-              {isDarkMode ? <RiMoonFill /> : <AiFillSun />}
-            </button>
-          </div>
-          <div className="flex mt-4 w-[500px]">
-            <input
-              type="text"
-              className="w-[400px] h-16 bg-white rounded-l-lg shadow-lg placeholder-gray-500 pl-4 outline-none"
-              placeholder="Add a new task"
-              value={newTaskText}
-              onChange={(e) => setNewTaskText(e.target.value)}
-              onKeyDown={handleKeyDown}
-            />
-            <button
-              className="w-[100px] h-16 bg-blue-500 text-white rounded-r-lg shadow-lg"
-              onClick={addTask}
-            >
-              Add Task
-            </button>
-          </div>
+      </div>
+
+      {/* Input and Toggle */}
+      <div className="relative flex flex-col items-center justify-center w-full px-4 sm:px-0 -mt-[25vh]">
+        <div className="flex justify-between items-center w-full sm:w-[500px]">
+          <h1 className="text-white text-4xl font-bold tracking-widest">TODO</h1>
+          <button className="text-white text-4xl" onClick={toggleDarkMode}>
+            {isDarkMode ? <RiMoonFill /> : <AiFillSun />}
+          </button>
+        </div>
+        <div className="flex mt-4 w-full sm:w-[500px]">
+          <input
+            type="text"
+            className="w-full sm:w-[400px] h-12 bg-white rounded-l-lg shadow-lg placeholder-gray-500 pl-4 outline-none"
+            placeholder="Create a new todo..."
+            value={newTaskText}
+            onChange={(e) => setNewTaskText(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <button
+            className="w-32 sm:w-[100px] h-12 bg-blue-700 text-white rounded-r-lg shadow-lg"
+            onClick={addTask}
+          >
+            Add Task
+          </button>
         </div>
       </div>
 
       {/* Task List */}
-      <div className="w-full flex justify-center mt-[-75px] relative z-10">
-        <div className={`bg-white ${isDarkMode ? 'bg-gray-900 text-white' : ''} w-[500px] shadow-lg rounded-lg p-4`}>
+      <div className="w-full flex justify-center mt-4 sm:mt-[20px] relative z-10 px-4 sm:px-0">
+        <div className={`bg-white ${isDarkMode ? 'bg-gray-900 text-white' : ''} w-full sm:w-[500px] shadow-lg rounded-lg p-4`}>
           <ul>
             {tasks.map((task) => (
               <li
                 key={task.id}
-                className={`flex items-center justify-between border-b py-4 ${isDarkMode ? 'border-gray-700' : ''}`}
+                className={`flex items-center justify-between border-b py-3 ${isDarkMode ? 'border-gray-700' : ''}`}
               >
                 <input
                   type="checkbox"
                   id={`checkbox${task.id}`}
-                  className="custom-checkbox"
+                  className="custom-checkbox rounded-full"
                   checked={task.completed}
                   onChange={() => toggleTaskCompletion(task.id)}
                 />
@@ -110,7 +113,7 @@ export default function Home() {
                   {task.text}
                 </label>
                 <button
-                  className="text-red-500"
+                  className="text-red-500 font-bold"
                   onClick={() => removeTask(task.id)}
                 >
                   <RxCross2 />
@@ -118,19 +121,19 @@ export default function Home() {
               </li>
             ))}
           </ul>
-          <div className="flex justify-between items-center mt-4">
+          <div className="flex flex-row sm:flex-row justify-between items-center mt-4 text-sm">
             <span>{tasks.filter((task) => !task.completed).length} items left</span>
-            <div>
-              <button className="mx-2 text-blue-500">All</button>
-              <button className="mx-2 text-blue-500">Active</button>
-              <button className="mx-2 text-blue-500">Completed</button>
-            </div>
             <button
-              className="text-blue-500"
+              className="text-blue-500 mt-2 sm:mt-0"
               onClick={() => setTasks(tasks.filter((task) => !task.completed))}
             >
               Clear Completed
             </button>
+          </div>
+          <div className="flex justify-center mt-4 sm:mt-0 bg-white sm:bg-transparent ${isDarkMode ? 'bg-gray-900 text-white' : ''} rounded-lg p-2 sm:p-0">
+            <button className="mx-2 text-blue-500">All</button>
+            <button className="mx-2 text-blue-500">Active</button>
+            <button className="mx-2 text-blue-500">Completed</button>
           </div>
         </div>
       </div>
